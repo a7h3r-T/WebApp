e0387e572551a332a40a0913e8d1419be8cb82be
a7h3rt <a7h3rt@SZA0030877>
2022-11-23 13:23:21 +0100

    gyakorlat

---
+++ WebApp/wwwroot/geek.js
@@ -0,0 +1,24 @@
+﻿var viccek;
+
+function letöltésBefejeződött(d) {
+    console.log("Sikeres letöltés")
+    console.log(d)
+    viccek = d;
+
+    for (var i = 0; i < viccek.length; i++) {
+        var sor = document.createElement("div")
+        sor.innerHTML = viccek[i].text;
+        document.body.appendChild(sor);
+    }
+}
+
+function letöltés() {
+    fetch('/jokes.json')
+        .then(response => response.json())
+        .then(data => letöltésBefejeződött(data));
+}
+
+window.onload = letöltés();
+
+
+
