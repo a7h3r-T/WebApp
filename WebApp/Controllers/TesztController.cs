9416e1aa10d4d2fbd69e378359b36eb55f1b3499
a7h3rt <a7h3rt@SZA0030877>
2022-11-30 12:35:05 +0100

    kérdések

---
+++ WebApp/Controllers/TesztController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Controllers
+{
+    //[Route("api/[controller]")]
+    [ApiController]
+    public class TesztController : ControllerBase
+    {
+        [HttpGet]
+        [Route("corvinus/szerverido")]
+        public IActionResult M1()
+        {
+            string pontosIdő = DateTime.Now.ToString();
+
+            return new ContentResult
+            {
+                ContentType = System.Net.Mime.MediaTypeNames.Text.Plain,
+                Content = pontosIdő
+            };
+
+            //return Ok(pontosIdő);
+        }
+
+        [HttpGet]
+        [Route("corvinus/nagybetus/{szoveg}")]
+        public IActionResult M2(string szoveg)
+        {
+            try
+            {
+                return new ContentResult
+                {
+                    ContentType = System.Net.Mime.MediaTypeNames.Text.Plain,
+                    Content = szoveg.ToUpper()
+
+                    //return Ok(szoveg.ToUpper());
+                };
+            }
+            catch (Exception)
+            {
+                return BadRequest("Nem jó a bemenő adat");
+            }
+             
+        }
+    }
+}
